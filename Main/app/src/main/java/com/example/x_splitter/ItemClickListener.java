e4d39c410aec0ccbd16ad6a2be5c9635851f7586
ruxanamaharjan <ruxanamaharjan@gmail.com>
2019-08-23 15:18:07 +0545

    update

--- Main/app/src/main/java/com/example/x_splitter/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.x_splitter;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onItemClick(View v, int pos);
-}
--- Main/app/src/main/java/com/example/x_splitter/SignUpRegisterValues.java
+++
@@ -1,7 +0,0 @@
-//package com.example.x_splitter;
-//
-//public class SignUpRegisterValues{
-//    String email;
-//    String username;
-//    String password;
-//}
